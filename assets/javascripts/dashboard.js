75e7608c27f3e9b35f1a46c13ed7fe276cb83bce
Pedro Sola <p3dro.sola@gmail.com>
2014-01-31 18:44:12 +0100

    Added new post types

--- assets/javascripts/dashboard.js
+++ assets/javascripts/dashboard.js
@@ -1,6 +1,8 @@
 (function (global) {
 
-  var eeenter = global.eeenter = {
+  var eeenter, div, n;
+
+  eeenter = global.eeenter = {
     posts: []
   , templates: { posts: {} }
   };
@@ -11,11 +13,11 @@
 
   };
 
-  eeenter.fetchPosts = function (callback) {
+  eeenter.fetchPosts = function (callback, offset) {
     var url, request;
     if (eeenter.fetching) return;
     eeenter.fetching = true;
-    url = '/dashboard/posts?offset=' + eeenter.posts.length + '&type=photo';
+    url = '/dashboard/posts?offset=' + (offset === undefined ? eeenter.posts.length : offset);
     request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.send();
@@ -44,25 +46,92 @@
     }
   };
 
-  eeenter.templates.posts.photo= function (post) {
-    var post_wrapper = document.createElement('div');
-    post_wrapper.classList.add('post');
-    post_wrapper.classList.add('post--photo');
-    post_wrapper['data-post-id'] =  post.id;
-    _.each(post.photos, function (photo) {
-        var img = document.createElement('img'),
-        photo_wrapper = document.createElement('div');
-        photo_wrapper.classList.add("post-photo-wrapper");
+  eeenter.countNew = function (callback) {
+    eeenter.fetchPosts(function (posts) {
+      var n = _.map(posts, function (p) {return p.id; }),
+      old = _.map(eeenter.posts, function (p) {return p.id; });
+      callback(_.difference(n, old).length);
+    }, 0);
+  };
+
+  // templates
+
+  eeenter.templates.postWrapper = function (post, children) {
+    return div({'class': 'post post--' + post.type, 'data-post-id': post.id}, children);
+  };
+
+  eeenter.templates.blogname = function (post) {
+    return div({ 'class': 'blogname'}, [
+      n('a', { href: post.post_url, target: '_blank'}, [post.blog_name])
+    ]);
+  };
+
+  eeenter.templates.tags = function (post) {
+    return div({'class':'tags'},
+      _.map(post.tags, function (tag) {
+        return '#' + tag + ' ';
+      }));
+  };
+
+  eeenter.templates.posts.photo = function (post) {
+    return eeenter.templates.postWrapper(post, [
+      div({'class': 'inner'}, _.map(post.photos, function (photo) {
+        var img = document.createElement('img');
+        // add load listener before setting src
         img.addEventListener('load', function () {
           _.defer(function () {
             img.classList.add('is-loaded');
           })
         });
         img.src = photo.alt_sizes[0].url;
-        photo_wrapper.appendChild(img);
-        post_wrapper.appendChild(photo_wrapper);
-      });
-    return post_wrapper;
+        var img = n('img', {src: photo.alt_sizes[0].url});
+        return n('div', {}, [img]);
+      }))
+    ]);
+  };
+
+  eeenter.templates.posts.text = function (post) {
+    return eeenter.templates.postWrapper(post, [
+      div({'class': 'inner'}, [
+        eeenter.templates.blogname(post),
+        div({}, eeenter.parseHTML(post.body)),
+        eeenter.templates.tags(post)
+        ])
+    ]);
+  };
+
+  eeenter.templates.posts.link = function (post) {
+    return eeenter.templates.postWrapper(post, [
+      div({'class': 'inner'}, [
+        eeenter.templates.blogname(post),
+        n('p', {}, [
+          n('a', {'class': 'post-link', href: post.url}, [post.title])
+        ])]
+        .concat(eeenter.parseHTML(post.description))
+        .concat([eeenter.templates.tags(post)])
+      )
+    ]);
+  };
+
+  eeenter.templates.posts.quote = function (post) {
+    return eeenter.templates.postWrapper(post, [
+      div({'class': 'inner'}, [
+        eeenter.templates.blogname(post),
+        eeenter.parseHTML("<blockquote>" + post.text + "</blockquote")[0],
+        n('p', {}, eeenter.parseHTML(post.source)),
+        eeenter.templates.tags(post)
+      ])
+    ]);
+  };
+
+  eeenter.templates.posts.video = function (post) {
+    return eeenter.templates.postWrapper(post, [
+      div({'class': 'inner'}, [
+        eeenter.templates.blogname(post),
+        n('p', {}, eeenter.parseHTML(_.last(post.player).embed_code)),
+        eeenter.templates.tags(post)
+      ])
+    ]);
   };
 
   // helpers
@@ -70,7 +139,7 @@
   eeenter.parseHTML = function(str) {
     var tmp = document.implementation.createHTMLDocument();
     tmp.body.innerHTML = str;
-    return tmp.body.children;
+    return _.toArray(tmp.body.children);
   };
 
   eeenter.appendToPage = function (dom_nodes) {
@@ -78,6 +147,26 @@
     _.each(dom_nodes, posts_wrapper.appendChild.bind(posts_wrapper));
   };
 
+
+  n = function(tagname, attributes, children) {
+    tagname = tagname || 'div';
+    attributes = attributes || {};
+    children = children || [];
+    var node = document.createElement(tagname);
+    _.each(attributes, function (val, key) {
+      node.setAttribute(key, val);
+    });
+    _.each(children, function (child) {
+      if (_.isString(child)) {
+        node.appendChild(document.createTextNode(child));
+      } else {
+        node.appendChild(child);
+      }
+    });
+    return node;
+  }
+
+  div = _.partial(n, 'div');
 
   document.addEventListener('DOMContentLoaded', eeenter.initialize);
 
